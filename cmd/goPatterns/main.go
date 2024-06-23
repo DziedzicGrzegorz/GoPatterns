bed20a11675f0d22afa76b3aeea80b0c4c5dfeb1
DziedzicGrzegorz <grzegorzdziedzic198@gmail.com>
2024-06-23 22:15:35 +0200

    feat(creational): singleton implementation

--- cmd/goPatterns/main.go
+++ cmd/goPatterns/main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"sync"
+
+	"github.com/dziedzicgrzegorz/goPatterns/internal/design/creational"
 	"github.com/dziedzicgrzegorz/goPatterns/internal/samplehash"
 )
 
 func main() {
 	samplehash.MakeHash("Grzegorz Dziedzic")
+
+	var wg sync.WaitGroup
+	const numGoroutines = 10
+	wg.Add(numGoroutines)
+
+	results := make(chan *creational.Single, numGoroutines)
+	for i := 0; i < numGoroutines; i++ {
+		go func() {
+			defer wg.Done()
+			results <- creational.GetInstance()
+		}()
+	}
+
+	wg.Wait()
+	close(results)
+	for result := range results {
+		fmt.Printf("Instance address: %p\n", result)
+	}
+
 }
