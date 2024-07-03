908a19b5305c901f1dacc51656403bb4bbd4ce45
Grzegorz Dziedzic <grzegorzdziedzic198@gmail.com>
2024-07-03 23:09:34 +0200

    clean main

--- cmd/goPatterns/main.go
+++ cmd/goPatterns/main.go
@@ -1,47 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"sync"
-
-	"github.com/dziedzicgrzegorz/goPatterns/internal/design/creational"
-	"github.com/dziedzicgrzegorz/goPatterns/internal/samplehash"
-	"go.uber.org/zap"
-)
-
 func main() {
-	samplehash.MakeHash("Grzegorz Dziedzic")
-
-	var wg sync.WaitGroup
-	const numGoroutines = 10
-	wg.Add(numGoroutines)
-
-	results := make(chan *creational.Single, numGoroutines)
-	for i := 0; i < numGoroutines; i++ {
-		go func() {
-			defer wg.Done()
-			results <- creational.GetInstance()
-		}()
-	}
-
-	wg.Wait()
-	close(results)
-	for result := range results {
-		fmt.Printf("Instance address: %p\n", result)
-	}
-	logger, err := zap.NewProduction()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	sugar := logger.Sugar()
-	defer logger.Sync()
-
-	sugar.Debug("this is a debug message")
-	sugar.Info("this is an info message")
-	sugar.Warn("this is a warn message")
-	sugar.Error("this is an error message")
-	sugar.Fatal("this is a fatal message")
 
 }
