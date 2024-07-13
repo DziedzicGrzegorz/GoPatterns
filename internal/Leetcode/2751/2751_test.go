4198e3d7833c77a9e59d1bca5df12d7b1d35d470
Grzegorz Dziedzic <grzegorzdziedzic198@gmail.com>
2024-07-14 00:46:50 +0200

    test(2751): Hard
    Add Bench test to test the optimization result

--- internal/Leetcode/2751/2751_test.go
+++ internal/Leetcode/2751/2751_test.go
@@ -1,8 +1,8 @@
 package _2751
 
 import (
+	"fmt"
 	"reflect"
-	_ "reflect"
 	"testing"
 )
 
@@ -50,6 +50,65 @@ func Test_survivedRobotsHealths(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := survivedRobotsHealths(tt.args.positions, tt.args.healths, tt.args.directions); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("survivedRobotsHealths() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func generateTestCases(limit int) []struct {
+	positions  []int
+	healths    []int
+	directions string
+} {
+	var testCases []struct {
+		positions  []int
+		healths    []int
+		directions string
+	}
+
+	for n := 1; n <= limit; n *= 2 {
+		testCases = append(testCases, struct {
+			positions  []int
+			healths    []int
+			directions string
+		}{
+			positions:  generateSequentialSlice(n),
+			healths:    generateSequentialSlice(n),
+			directions: generateDirections(n),
+		})
+	}
+
+	return testCases
+}
+
+func generateSequentialSlice(n int) []int {
+	slice := make([]int, n)
+	for i := 0; i < n; i++ {
+		slice[i] = i + 1
+	}
+	return slice
+}
+
+func generateDirections(n int) string {
+	directions := make([]byte, n)
+	for i := 0; i < n; i++ {
+		if i%2 == 0 {
+			directions[i] = 'R'
+		} else {
+			directions[i] = 'L'
+		}
+	}
+	return string(directions)
+}
+
+func Benchmark_survivedRobotsHealths(b *testing.B) {
+	limit := 128
+	testCases := generateTestCases(limit)
+
+	for _, v := range testCases {
+		b.Run(fmt.Sprintf("length_%d", len(v.positions)), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				survivedRobotsHealths(v.positions, v.healths, v.directions)
 			}
 		})
 	}
